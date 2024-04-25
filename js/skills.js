14d9f46df44c23d5c45cb88ca9f54a54ef0da7bd
Kassandra Ortega <49657598+kortega18@users.noreply.github.com>
2024-04-24 20:03:33 -0500

    Initial Commit
    Base of my website

---
+++ js/skills.js
@@ -0,0 +1,502 @@
+const skills_list = [
+    {
+        pic: "",
+        name: "Assembly", 
+        type: "Programming Language", 
+        rate: "\u2B50", 
+        dots: "︙"
+    },
+    {
+        pic: "",
+        name: "Agile", 
+        type: "Methodologies", 
+        rate: "\u2B50 \u2B50 \u2B50 \u2B50 \u2B50", 
+        dots: "︙"
+    },
+    {
+        pic: "",
+        name: "Android", 
+        type: "Software Development", 
+        rate: "\u2B50 \u2B50 \u2B50 \u2B50 \u2B50", 
+        dots: "︙"
+    },
+    {
+        pic: "",
+        name: "API", 
+        type: "Software Development", 
+        rate: "\u2B50 \u2B50 \u2B50 \u2B50", 
+        dots: "︙"
+    },
+    {
+        pic: "",
+        name: "Application", 
+        type: "Software Development", 
+        rate: "\u2B50 \u2B50 \u2B50 \u2B50 \u2B50", 
+        dots: "︙"
+    },
+    {
+        pic: "",
+        name: "BASH", 
+        type: "Programming Language", 
+        rate: "\u2B50 \u2B50 \u2B50",
+        dots: "︙"
+    },
+    {
+        pic: "",
+        name: "Backend", 
+        type: "Software Development", 
+        rate: "\u2B50 \u2B50 \u2B50 \u2B50 \u2B50", 
+        dots: "︙"
+    },
+    {
+        pic: "",
+        name: "C", 
+        type: "Programming Language", 
+        rate: "\u2B50 \u2B50 \u2B50", 
+        dots: "︙"
+    },
+    {
+        pic: "",
+        name: "C#", 
+        type: "Programming Language", 
+        rate: "\u2B50 \u2B50 \u2B50 \u2B50 \u2B50", 
+        dots: "︙"
+    },
+    {
+        pic: "",
+        name: "C++", 
+        type: "Programming Language", 
+        rate: "\u2B50 \u2B50 \u2B50 \u2B50 \u2B50", 
+        dots: "︙"
+    },
+    {
+        pic: "",
+        name: "CSS", 
+        type: "Programming Language", 
+        rate: "\u2B50 \u2B50 \u2B50 \u2B50 \u2B50", 
+        dots: "︙"
+    },
+    {
+        pic: "",
+        name: "Database", 
+        type: "Software Development", 
+        rate: "\u2B50 \u2B50 \u2B50 \u2B50", 
+        dots: "︙"
+    },
+    {
+        pic: "",
+        name: "DevOps", 
+        type: "Methodologies", 
+        rate: "\u2B50 \u2B50 \u2B50 \u2B50 \u2B50", 
+        dots: "︙"
+    },
+    {
+        pic: "",
+        name: "Frontend", 
+        type: "Software Development", 
+        rate: "\u2B50 \u2B50 \u2B50 \u2B50", 
+        dots: "︙"
+    },
+    {
+        pic: "",
+        name: "HTML", 
+        type: "Programming Language", 
+        rate: "\u2B50 \u2B50 \u2B50 \u2B50 \u2B50", 
+        dots: "︙"
+    },
+    {
+        pic: "",
+        name: "iOS", 
+        type: "Software Development", 
+        rate: "\u2B50 \u2B50 \u2B50 \u2B50", 
+        dots: "︙"
+    },
+    {
+        pic: "",
+        name: "Kotlin", 
+        type: "Programming Language", 
+        rate: "\u2B50 \u2B50 \u2B50", 
+        dots: "︙"
+    },
+    {
+        pic: "",
+        name: "Java", 
+        type: "Programming Language", 
+        rate: "\u2B50 \u2B50 \u2B50 \u2B50 \u2B50", 
+        dots: "︙"
+    },
+    {
+        pic: "",
+        name: "JavaScript", 
+        type: "Programming Language", 
+        rate: "\u2B50 \u2B50 \u2B50 \u2B50 \u2B50", 
+        dots: "︙"
+    },
+    {
+        pic: "",
+        name: "JQuery", 
+        type: "Programming Language", 
+        rate: "\u2B50 \u2B50 \u2B50 \u2B50", 
+        dots: "︙"
+    },
+    {
+        pic: "",
+        name: "Mobile", 
+        type: "Software Development", 
+        rate: "\u2B50 \u2B50 \u2B50 \u2B50 \u2B50", 
+        dots: "︙"
+    },
+    {
+        pic: "",
+        name: "Objective-C", 
+        type: "Programming Language", 
+        rate: "\u2B50 \u2B50 \u2B50 \u2B50", 
+        dots: "︙"
+    },
+    {
+        pic: "",
+        name: "PHP", 
+        type: "Programming Language", 
+        rate: "\u2B50 \u2B50 \u2B50", 
+        dots: "︙"
+    },
+    {
+        pic: "",
+        name: "Project Manager", 
+        type: "Roles", 
+        rate: "\u2B50 \u2B50 \u2B50 \u2B50 \u2B50", 
+        dots: "︙"
+    },
+    {
+        pic: "",
+        name: "Project Owner", 
+        type: "Roles", 
+        rate: "\u2B50 \u2B50 \u2B50 \u2B50 \u2B50", 
+        dots: "︙"
+    },
+    {
+        pic: "",
+        name: "Python", 
+        type: "Programming Language", 
+        rate: "\u2B50 \u2B50 \u2B50 \u2B50", 
+        dots: "︙"
+    },
+    {
+        pic: "",
+        name: "QA Engineer", 
+        type: "Roles", 
+        rate: "\u2B50 \u2B50 \u2B50", 
+        dots: "︙"
+    },
+    {
+        pic: "",
+        name: "R", 
+        type: "Programming Language", 
+        rate: "\u2B50 \u2B50", 
+        dots: "︙"
+    },
+    {
+        pic: "",
+        name: "Software Developer", 
+        type: "Roles", 
+        rate: "\u2B50 \u2B50 \u2B50 \u2B50 \u2B50", 
+        dots: "︙"
+    },
+    {
+        pic: "",
+        name: "SQL", 
+        type: "Programming Language", 
+        rate: "\u2B50 \u2B50 \u2B50", 
+        dots: "︙"
+    },
+    {
+        pic: "",
+        name: "Swift", 
+        type: "Programming Language", 
+        rate: "\u2B50 \u2B50 \u2B50 \u2B50 \u2B50", 
+        dots: "︙"
+    },
+    {
+        pic: "",
+        name: "TypeScript", 
+        type: "Programming Language", 
+        rate: "\u2B50 \u2B50", 
+        dots: "︙"
+    },
+    {
+        pic: "",
+        name: "UI/UX Designer", 
+        type: "Roles", 
+        rate: "\u2B50 \u2B50 \u2B50", 
+        dots: "︙"
+    },
+    {
+        pic: "",
+        name: "Waterfall", 
+        type: "Methodologies", 
+        rate: "\u2B50 \u2B50 \u2B50 \u2B50", 
+        dots: "︙"
+    },
+    {
+        pic: "",
+        name: "Web", 
+        type: "Software Development", 
+        rate: "\u2B50 \u2B50 \u2B50 \u2B50 \u2B50", 
+        dots: "︙"
+    },
+];
+
+var table = document.getElementById("skills-table");
+var itemsPerTable = document.getElementById("input-number");
+var hideChangePageArea = document.getElementById("change-pages");
+var hideAndShow = document.getElementById("show-all");
+var table = document.getElementById("skills-table");
+var skills_section = document.getElementById("skills-section");
+var search = document.getElementById("search-icon");
+var rowCount;
+
+/************ Show Entire List *************/
+function showAll() {
+    rowCount = table.rows.length;
+    if(hideAndShow.innerText == "Show All") {
+        hideAndShow.innerHTML = "Hide Some";
+        hideChangePageArea.style.visibility = "hidden";
+
+        for (let i = rowCount-1; i > 0; i--){
+            table.deleteRow(i);
+        }
+
+        showAllSkills();
+    }
+    else {
+        hideAndShow.innerHTML = "Show All";
+        hideChangePageArea.style.visibility = "visible";
+
+        changePage(1, false);
+
+        skills_section.scrollIntoView();
+    }
+}
+
+/************ Whole Table of Skills *************/
+function showAllSkills() {    
+    for(skill of skills_list) {
+        // Create row
+        let row = document.createElement("tr");
+        
+        // Create cells
+        let c1 = document.createElement("td");
+        let c2 = document.createElement("td");
+        let c3 = document.createElement("td");
+        let c4 = document.createElement("td");
+        let c5 = document.createElement("td");
+
+        // Insert data to cells
+        c1.innerHTML = skill.pic;
+        c2.innerText = skill.name;
+        c3.innerText = skill.type;
+        c4.innerText = skill.rate;
+        c5.innerText = skill.dots;
+
+        // Append cells to row
+        row.appendChild(c1);
+        row.appendChild(c2);
+        row.appendChild(c3);
+        row.appendChild(c4);
+        row.appendChild(c5);
+
+        // Append row to table body
+        table.appendChild(row);
+    }
+}
+
+/************ Change Page *************/
+var current_page = 1;
+var records_per_page = itemsPerTable.value;
+
+function prevPage()
+{
+    if (current_page > 1) {
+        current_page--;
+        changePage(current_page, false);
+    }
+}
+
+function nextPage()
+{
+    if (current_page < numPages()) {
+        current_page++;
+        changePage(current_page, false);
+    }
+}
+
+function changePage(page, onLoad)
+{
+    var btn_prev = document.getElementById("btn_prev");
+    var btn_next = document.getElementById("btn_next");
+    var page_span = document.getElementById("page");
+    rowCount = table.rows.length;
+
+    // Validate page
+    if (page < 1) page = 1;
+    if (page > numPages()) page = numPages();        
+
+    if(onLoad != true){
+        for (let i = rowCount-1; i > 0; i--){
+            table.deleteRow(i);
+        }
+        onLoad = false;
+    }
+
+    for (var i = (page-1) * records_per_page; i < (page * records_per_page) && i < skills_list.length; i++) {
+        // Create row
+        let row = document.createElement("tr");
+
+        // Create cells
+        let c1 = document.createElement("td");
+        let c2 = document.createElement("td");
+        let c3 = document.createElement("td");
+        let c4 = document.createElement("td");
+        let c5 = document.createElement("td");
+
+        // Insert data to cells
+        c1.innerHTML = skills_list[i].pic;
+        c2.innerText = skills_list[i].name;
+        c3.innerText = skills_list[i].type;
+        c4.innerText = skills_list[i].rate;
+        c5.innerText = skills_list[i].dots;
+
+        // Append cells to row
+        row.appendChild(c1);
+        row.appendChild(c2);
+        row.appendChild(c3);
+        row.appendChild(c4);
+        row.appendChild(c5);
+
+        // Append row to table body
+        table.appendChild(row);
+    }
+    page_span.innerHTML = page;
+
+    if (page == 1) {
+        btn_prev.style.opacity = "0.2";
+    } 
+    else {
+        btn_prev.style.opacity = "1.0";
+    }
+
+    if (page == numPages()) {
+        btn_next.style.opacity = "0.2";
+    } 
+    else {
+        btn_next.style.opacity = "1.0";
+    }
+}
+
+function numPages()
+{
+    return Math.ceil(skills_list.length / records_per_page);
+}
+
+window.onload = function() {
+    changePage(1, true);
+};
+
+/************ Number Input and Controls *************/
+(function() {
+    window.inputNumber = function(el) {
+
+        var min = el.attr('min') || false;
+        var max = el.attr('max') || false;
+  
+        var els = {};
+  
+        els.dec = el.prev();
+        els.inc = el.next();
+    
+        el.each(function() {
+            init($(this));
+        });
+  
+        function init(el) {
+  
+            els.dec.on('click', decrement);
+            els.inc.on('click', increment);
+  
+            function decrement() {
+                var value = el[0].value;
+                    value--;
+                if(!min || value >= min) {
+                    el[0].value = value;
+                }
+                updateTable();
+            }
+  
+            function increment() {
+                var value = el[0].value;
+                value++;
+                if(!max || value <= max) {
+                    el[0].value = value++;
+                }
+                updateTable();
+            }
+
+            function updateTable() {
+                records_per_page = el[0].value;
+                if (current_page == 1) {
+                    changePage(1, false);
+                }
+                else {
+                    for (let i = 1; i < current_page; i++){
+                        changePage(i, false);
+                    }
+                }
+            }
+        }
+    }
+})();
+  
+inputNumber($('.input-number'));
+
+/************ SEARCH ICON *************/
+// ---- ---- Const ---- ---- //
+let inputBox = document.querySelector('.input-box'),
+    searchIcon = document.querySelector('.search'),
+    closeIcon = document.querySelector('.close-icon');
+
+// ---- ---- Open Input ---- ---- //
+searchIcon.addEventListener('click', () => {
+    inputBox.classList.add('open');
+});
+// ---- ---- Close Input ---- ---- //
+closeIcon.addEventListener('click', () => {
+    inputBox.classList.remove('open');
+});
+
+/************ SEARCH SKILLS FUNCTIONALITY *************/
+function searchSkills() {
+    var input, filter, tr, td, i, txtValue;
+    input = document.getElementById("user-search-input");
+    if(input.value.length != 0){
+        hideAndShow.style.visibility = "hidden";
+        hideChangePageArea.style.visibility = "hidden";
+    }
+    else {
+        hideAndShow.style.visibility = "visible";
+        hideChangePageArea.style.visibility = "visible";
+    }
+    filter = input.value.toUpperCase();
+    tr = table.getElementsByTagName("tr");
+    for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[1];
+        if (td) {
+            txtValue = td.textContent || td.innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            } 
+            else {
+                tr[i].style.display = "none";
+            }
+        }       
+    }
+}
